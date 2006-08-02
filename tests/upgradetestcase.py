64a6b90c6de8f94f603aeb774db0292efc5dd1e7
gracinet <devnull@localhost>
2006-08-02 16:27:12 +0000

    upgrades runner wasn't up to date wrt to upgrade categories

--- tests/upgradetestcase.py
+++ tests/upgradetestcase.py
@@ -17,12 +17,12 @@ for product in products:
     installProduct(product[1], 1)
 
 class UpgradeTestCase(ZopeTestCase):
-    
+
     # We need to have thee name of the directory that keeps the Data.fs,
     # so we can check that the test is run with the right Data.fs. Since
     # each test test a specific CPS version, you would get false errors
     # if run with the wrong Data.fs.
-    db_dir = None 
+    db_dir = None
 
     def beforeSetUp(self):
         self._checkDB()
@@ -36,12 +36,16 @@ class UpgradeTestCase(ZopeTestCase):
     def _createSetupTool(self):
         self.app.cps.manage_addProduct['CPSCore'].manage_addTool(
             CPSSetupTool.meta_type)
-        
+
     def _doAllUpgrades(self):
+        from Products.CPSCore.upgrade import _categories_registry
+        categories = _categories_registry.keys()
         setuptool = self.app.cps.portal_setup
-        upgrades = [u['id'] for u in setuptool.listUpgrades() if u['proposed']]
-        if upgrades:
-            setuptool.doUpgrades(upgrades)        
+        for cat in categories:
+            upgrades = [u['id'] for u in setuptool.listUpgrades(category=cat)
+                        if u['proposed']]
+            if upgrades:
+                setuptool.doUpgrades(upgrades, cat)
 
     def _installDefaultProfile(self):
         setuptool = self.app.cps.portal_setup
@@ -60,10 +64,10 @@ class UpgradeTestCase(ZopeTestCase):
             if profile in all_profiles:
                 profiles.append(profile)
                 all_profiles.remove(profile)
-        
+
         # The rest can be added to the end.
         profiles.extend(all_profiles)
-        
+
         for profile in profiles:
             # Install profiles
             setuptool.importProfile('profile-'+profile)
@@ -73,7 +77,7 @@ class UpgradeTestCase(ZopeTestCase):
     def _createSnapshot(self, id='snappy'):
         setuptool = self.app.cps.portal_setup
         setuptool.createSnapshot(id)
-        
+
     def _checkDB(self):
         if self.db_dir is None:
             raise "You have not set the db_dir attribute on your test case"
@@ -94,12 +98,12 @@ class UpgradeTestCase(ZopeTestCase):
         manager_attendee = manager_cal.getMainAttendee()
         events = manager_attendee.getEvents((None,None))
         self.failUnlessEqual(len(events), 2)
-        
+
         for event in events:
             if event.title == 'Private Event':
                 # Make sure the private event is private
                 self.failUnlessEqual(event.access, u'PRIVATE')
-                self.failUnlessEqual(event.document, 
+                self.failUnlessEqual(event.document,
                                      '/cps/workspaces/calendar_document')
             elif event.title == 'Meeting':
                 # The meeting should have three attendees
@@ -111,7 +115,7 @@ class UpgradeTestCase(ZopeTestCase):
                     'CALENDAR!/cps/workspaces/workspace_calendar' in attendees)
                 self.failUnless(
                     'CALENDAR!/cps/workspaces/resource_calendar' in attendees)
-        
+
         # There should be both workspace and resource calendars as well.
         rescalendar = self.app.cps.workspaces.resource_calendar
         self.failUnlessEqual(rescalendar._attendee_type, 'RESOURCE')
@@ -121,25 +125,25 @@ class UpgradeTestCase(ZopeTestCase):
         wsattendee = wscalendar.getMainAttendee()
         events = wsattendee.getEvents((None,None))
         self.failUnlessEqual(len(events), 2)
-        
+
         occurrences = ()
         for event in events:
             if event.title == 'Recurring Event':
                 occurrences = event.expand((None, datetime.datetime.max))
         self.failUnlessEqual(len(occurrences), 5)
 
-        
+
     def _verifyDocument(self):
         doc = self.app.cps.workspaces.test_workspace.test_document
         content = doc.getContent()
         if getattr(content, 'content', None) is not None:
             # CPS 3.2.4
-            main = content.content 
+            main = content.content
             right = content.content_right
             photo = content.photo
         else:
             # CPS 3.3.8
-            main = content.content_f0 
+            main = content.content_f0
             right = content.content_right_f0
             photo = content.photo_f0
         self.failUnlessEqual(main, 'Main text')
@@ -149,7 +153,7 @@ class UpgradeTestCase(ZopeTestCase):
     def _verifyNewsItem(self):
         # Make sure News Items are now flexible, and that the file
         # attachement was included.
-        # This change happened in version 3.3.1, and 
+        # This change happened in version 3.3.1, and
         # the test is not necessary for later versions.
         doc = self.app.cps.workspaces.news_with_file
         content = doc.getContent()
@@ -163,7 +167,7 @@ class UpgradeTestCase(ZopeTestCase):
     def _verifyPublishing(self):
         doc = self.app.cps.workspaces.test_workspace.test_document
         content = doc.getContent().aq_base
-        
+
         root_section = self.app.cps.sections
         test_section = root_section.test_section
         self.failUnless(root_section.test_document.getContent().aq_base
@@ -172,22 +176,23 @@ class UpgradeTestCase(ZopeTestCase):
                         is content)
 
 
+
 class PreGenericSetupTestCase(UpgradeTestCase):
     """Tests upgrades from versions of CPS before Genericsetup support
     """
-                
+
     def _upgrade(self):
         # 1. Create the portal_setup tool
         self._createSetupTool()
-        
+
         # 2. Go to the portal_setup and run all proposed upgrades.
         self._doAllUpgrades()
         # The user folder has now been replaced, so we need to login again.
         self.login('manager')
-        
+
         # 3. Install the default profile.
         self._installDefaultProfile()
-        
+
         # 4. Set the membersfolder to it's old location:
         self.app.cps.portal_membership.setMembersFolderById(
             'workspaces/members')
@@ -198,7 +203,7 @@ class PreGenericSetupTestCase(UpgradeTestCase):
         # 6. Install all available extension profiles, and see if there
         #    are new upgrades to do after each profile install.
         self._installAllProfilesAndUpgrade()
-            
+
         # 7. Finally, we make a snapshot
         self._createSnapshot()
 
@@ -209,10 +214,10 @@ class PostGenericSetupTestCase(UpgradeTestCase):
     def _upgrade(self):
         # 1. Create a before upgrade snapshot.
         self._createSnapshot('before-upgrade')
-        
+
         # 2. Go to the portal_setup and run all proposed upgrades.
         self._doAllUpgrades()
-            
+
         # 3. Finally, we make a snapshot
         self._createSnapshot('after-upgrade')
-                
+
